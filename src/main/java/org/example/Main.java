21726e2a64be87b6c353c85976b833efadf7314b
noahissah <noah7issah@gmail.com>
2025-10-13 11:26:06 +0200

    Modif Noah

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        IO.println(String.format("Hello and welcome NOAH!"));
         IO.println(String.format("change"));
 
         for (int i = 1; i <= 5; i++) {
