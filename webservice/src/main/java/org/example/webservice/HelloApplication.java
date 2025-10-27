c42814e7a53493c0d60d64a7f84284c5b8eb1c0c
Sami <samibelakhdar200801@gmail.com>
2025-10-27 10:00:35 +0100

    modif

---
+++ webservice/src/main/java/org/example/webservice/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.webservice;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
